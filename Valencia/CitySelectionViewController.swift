f3cbc947067b9f9f4b54844c9897d4be294993f1
Eunjin Kim <jagpavv@gmail.com>
2018-01-30 18:50:48 +0100

    Add searching in city list ( squashed )

--- Valencia/CitySelectionViewController.swift
+++ Valencia/CitySelectionViewController.swift
@@ -4,27 +4,46 @@ protocol CitySelectionProtocol {
   func citySelected(city: String)
 }
 
-class CitySelectionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class CitySelectionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
   var delegate: CitySelectionProtocol?
   let sectionTitles = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+  var timeZoneCity: [String] = []
+  var timeZoneCityFiltered: [String] = []
+
   var sectionValues: [[String]] = []
+
+  var searchString = ""
+  var isSearching: Bool {
+    return searchString.count > 0
+  }
+
+  @IBOutlet weak var cityListSearchBar: UISearchBar!
+  @IBOutlet weak var cityListTableView: UITableView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
     let timeZone = TimeZone.knownTimeZoneIdentifiers
-    var timeZoneCity: [String] = [].sorted(by: < )
     for identifier in timeZone {
       if let cityName = identifier.split(separator: "/").last {
         timeZoneCity.append("\(cityName)")
       }
     }
 
+    timeZoneCity = timeZoneCity.sorted()
+
     for keyword in sectionTitles {
       let result = timeZoneCity.filter { $0.hasPrefix(keyword) }
       sectionValues.append(result)
     }
+  }
+
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    searchString = searchText
+    timeZoneCityFiltered = timeZoneCity.filter { $0.lowercased().contains(searchString.lowercased()) }
+    cityListTableView.reloadData()
   }
 
   @IBAction func cancelTapped(_ sender: UIButton) {
@@ -33,30 +52,31 @@ class CitySelectionViewController: UIViewController, UITableViewDelegate, UITabl
 
 
   func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-    return sectionTitles[section]
+    return isSearching ? nil : sectionTitles[section]
   }
 
   func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-    return sectionTitles
+    return isSearching ? nil : sectionTitles
   }
 
   func numberOfSections(in tableView: UITableView) -> Int {
-    return sectionTitles.count
+    return isSearching ? 1 : sectionTitles.count
   }
 
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return sectionValues[section].count
+    return isSearching ? timeZoneCityFiltered.count : sectionValues[section].count
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "TimeZoneCell")
-    cell?.textLabel?.text = sectionValues[indexPath.section][indexPath.row]
+    cell?.textLabel?.text =
+      isSearching ? timeZoneCityFiltered[indexPath.row] : sectionValues[indexPath.section][indexPath.row]
     return cell!
   }
 
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    delegate?.citySelected(city: sectionValues[indexPath.section][indexPath.row])
+    let selectedCity = isSearching ? timeZoneCityFiltered[indexPath.row] : sectionValues[indexPath.section][indexPath.row]
+    delegate?.citySelected(city: selectedCity)
     dismiss(animated: true, completion: nil)
   }
-
 }
